76201cbc21c35decd51084da9996bd5f02ef5c34
ChristinaIbrahim <christina.i.aziz7@gmail.com>
2025-11-29 01:29:51 +0200

    create Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { AnnouncementBar } from "./components/announcement-bar/announcement-bar";
+import { Header } from "./components/header/header";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, AnnouncementBar, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Header } from "../header/header";
+import { AnnouncementBar } from "../announcement-bar/announcement-bar";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, Header, AnnouncementBar],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
