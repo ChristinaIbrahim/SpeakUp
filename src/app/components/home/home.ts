c52f73260ca370e045313d9737f2d8fd92a024e8
ChristinaIbrahim <christina.i.aziz7@gmail.com>
2025-12-01 21:41:51 +0200

    Create Pricing & Footer Section

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -3,10 +3,13 @@ import { Navbar } from "../navbar/navbar";
 import { Header } from "../header/header";
 import { Mainsection } from './mainsection/mainsection';
 import { Descreption } from './descreption/descreption';
+import { Testimonials } from "./testimonials/testimonials";
+import { Pricing } from "./pricing/pricing";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Header,Mainsection,Descreption],
+  imports: [Navbar, Header, Mainsection, Descreption, Testimonials, Pricing,Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: ["./src/**/*.{html,ts}"],
   plugins: {
     daisyui: {},
 
   },
+   theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  }
 };
