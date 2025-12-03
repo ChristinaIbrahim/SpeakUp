11dd5977506aa82c30ee5d1459c464e0e4d479cb
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-12-03 22:30:32 +0200

    Login Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Header } from "./components/header/header";
 import { Home } from "./components/home/home";
 import { Mainsection } from './components/home/mainsection/mainsection';
 import { Descreption } from './components/home/descreption/descreption';
+import { LoginComponent } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Header, Home  ,Mainsection,Descreption],
+  imports: [RouterOutlet, Navbar, Header, Home  ,Mainsection,Descreption,LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
